04e5b5ff6ab08454989830d3dd78871cdcbb7308
trxsh1DEV <yagosousa630@gmail.com>
2024-06-02 20:26:03 -0300

    fixed problem for connect socket using method socket.bind, was added one line for get ip and port based at the machine that are executing code

--- agent.py
+++ agent.py
@@ -6,23 +6,20 @@ import platform
 import psutil
 import subprocess
 from hashlib import md5
-# from bson import ObjectId
 from uuid import uuid4
 import tempfile
 import wmi
 import geocoder
 
+c = wmi.WMI()
 
 def get_workstation_domain_wmi():
-    c = wmi.WMI()
     for computer in c.Win32_ComputerSystem():
         return computer.Domain
     
 def get_location():
     try:
-        # Obtém a localização atual usando a API de geolocalização do provedor padrão
         localizacao = geocoder.ip('me')
-
         if localizacao.ok:
             latitude, longitude = localizacao.latlng
             return {"latitude": latitude, "longitude": longitude}
@@ -34,15 +31,10 @@ def get_location():
         return None
 
 def get_hwid():
-    # Obtém informações do hardware que podem ser usadas para gerar o HWID
     try:
-        # Executa o comando wmic para obter informações do hardware
         wmic_output = subprocess.check_output('wmic csproduct get uuid', shell=True)
-        # Converte a saída para uma string decodificada
         wmic_output = wmic_output.decode('utf-8').strip()
-        # Calcula o hash MD5 das informações do hardware
         hwid = md5(wmic_output.encode('utf-8')).hexdigest()
-        # Convertendo string hwid em um JSON válido
         return hwid
     except Exception as e:
         print("Erro ao obter o HWID:", e)
@@ -50,16 +42,9 @@ def get_hwid():
 
 def get_device_info():
     c = wmi.WMI()
-    
-    device_info = {
-        "keyboard": {},
-        "mouse": {},
-        "monitors": []
-    }
-
+    device_info = {"keyboard": {}, "mouse": {}, "monitors": []}
     try:
         info_teclado = c.Win32_Keyboard()
-
         if info_teclado:
             for teclado in info_teclado:
                 device_info["keyboard"] = {
@@ -69,7 +54,6 @@ def get_device_info():
                 }
 
         info_mouse = c.Win32_PointingDevice()
-
         if info_mouse:
             for mouse in info_mouse:
                 device_info["mouse"] = {
@@ -80,115 +64,29 @@ def get_device_info():
 
         info_monitor = c.Win32_DesktopMonitor()
         info_video = c.Win32_VideoController()
-
-        # Se houver informações sobre os monitores, adicione ao objeto de retorno
         if info_monitor:
             c = wmi.WMI(namespace=r"root\wmi")
             query_result = c.query("SELECT * FROM WmiMonitorID")
             for query_result in query_result:
                 device_info["monitors"].append({
                     "edid": query_result.InstanceName.split("\\")[1],
-                    "resolution": str(info_video[0].CurrentHorizontalResolution) + " x " + str(info_video[0].CurrentVerticalResolution) + " - " + str(info_video[0].MaxRefreshRate) + " Hertz",
+                    "resolution": f"{info_video[0].CurrentHorizontalResolution} x {info_video[0].CurrentVerticalResolution} - {info_video[0].MaxRefreshRate} Hertz",
                     "gpu": info_video[0].Name,
                     "gpu_id": info_video[0].PNPDeviceID.split("\\")[-1],
                 })
 
     except Exception as e:
         print("Ocorreu um erro ao tentar obter informações sobre os dispositivos:", e)
-
     return device_info
-    
-def run_client():
-    server_ip = "192.168.0.2"  # Endereço IP do cliente
-    server_port = 8080  # Porta para o servidor socket
-
-    # Conexão com o servidor
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((server_ip, server_port))
-    server_port = client.getsockname()[1]
-
-    hwid = get_hwid()        
-    uid = read_from_registry() or write_to_regedit(str(uuid4()))
-    start_collect(client, {"hwid": hwid, "uid": uid})    
-
-    print("send")
-    client.close()
-
-    while True:
-        try:
-            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server.bind((server_ip, server_port))
-            server.listen(1)
-
-            print("Server is waiting for connection...")
-
-            # Aguarda até que uma conexão seja aceita
-            client, client_address = server.accept()
-            print(f"Connected to client: {client_address}")
-
-            print("Connected to server")
-                # Recebe mensagem do servidor
-            command = client.recv(1024).decode()
-            print("command", command)
-            
-            # Executa o comando recebido do servidor
-            if command.strip().lower() == "shutdown_now":
-                print("Recebido comando de desligamento. Desligando a máquina...")
-                subprocess.run(["shutdown", "-p"], shell=True)
-            elif command.strip().lower() == "get_inventory":
-                print("get_inventory")
-                start_collect(client, {"hwid": hwid, "uid": uid})
-            elif command.strip().lower() == "quit":
-                print("saindo...")
-                break  # Sai do loop e encerra a conexão com o servidor
-            elif command.strip().lower() == "winget_install":
-                subprocess.run("powershell -command \"iex ((New-Object System.Net.WebClient).DownloadString('https://aka.ms/install-winget'))\"")
-            elif command.strip().lower() == "remove_agent":
-                # wmic product where "name='Exemplo'" call uninstall
-                subprocess.Popen(["wmic product where name='nameApp' call uninstall"])
-            else:
-                timeout = command.split("##")[1] if len(command.split("##")) > 1 else 30
-                command = command.split("##")[0]
-
-                if "echo" in command:
-                    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.bat') as temp_file:
-                        temp_file.write(command)
-                        command = temp_file.name
-
-                encoding = "cp850" if platform.system() == "Windows" else "utf_8"
-                try:
-                    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True, encoding=encoding)
-                    # Aguarde até que o processo seja concluído
-                    stdout, stderr = process.communicate(timeout=int(timeout))
-                    if stdout:
-                        # print(stdout)
-                        client.sendall(stdout.encode())
-                    if stderr:
-                        client.sendall(stderr.encode())
-                        print("Erro ao executar o comando:", stderr)
-                except subprocess.TimeoutExpired:
-                    print("Tempo limite excedido ao executar o comando.")
-                except Exception as e:
-                    print("Erro ao executar o comando:", e)
-
-        except Exception as e:
-            print(f"Erro: {e}")
-        finally:
-            # Fecha a conexão com o cliente
-            client.close()
-            print("Connection to client closed")
 
 def write_to_regedit(data):
     try:
-        # Abrindo a chave do registro
         key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\AgentZer0", 0, winreg.KEY_WRITE | winreg.KEY_WOW64_64KEY)
     except FileNotFoundError:
-        # Se a chave não existe, crie-a
         key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\AgentZer0")
     
     value_name = "identity"
     value_type = winreg.REG_SZ
-    # Criando uid no campo "identity"
     winreg.SetValueEx(key, value_name, 0, value_type, data)
     winreg.CloseKey(key)
     return data
@@ -198,36 +96,21 @@ def read_from_registry():
         key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\AgentZer0", 0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY)        
         value, _ = winreg.QueryValueEx(key, "identity")        
         winreg.CloseKey(key)
-        
         return value
     except FileNotFoundError:
         return None
 
-# def get_computer_manufacturer():
-#     try:
-#         key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SYSTEM\HardwareConfig\Current")
-#         value, _ = winreg.QueryValueEx(key, "SystemManufacturer")
-#         winreg.CloseKey(key)
-#         if(value == "Default string"):
-#             return "Generic"
-#         return value.strip()
-#     except Exception as e:
-#         print("Erro ao obter o fabricante do computador:", e)
-#         return "Not Detected"
-    
 def get_version_system():
     try:
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion")
-        value = winreg.QueryValueEx(key, "ProductName")[0] 
-        winreg.CloseKey(key)
-        return value + " x" + platform.machine()[-2:]
+        for os in c.Win32_OperatingSystem():
+            return os.Caption
     except Exception as e:
         return str(e), None
 
 def categorie_system():
-    if(get_version_system().lower() in "server"):
+    if "server" in get_version_system().lower():
         return "Server"
-    elif(psutil.sensors_battery() is not None):
+    elif psutil.sensors_battery() is not None:
         return "Notebook"
     else:
         return "Desktop"
@@ -235,37 +118,31 @@ def categorie_system():
 def get_motherboard():
     try:
         chave = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"HARDWARE\DESCRIPTION\System\BIOS")
-        fabricante = winreg.QueryValueEx(chave, "BaseBoardManufacturer")[0]
+        model_extend = winreg.QueryValueEx(chave, "SystemProductName")[0]
         model = winreg.QueryValueEx(chave, "BaseBoardProduct")[0]
-        manufacturer = winreg.QueryValueEx(chave, "SystemManufacturer")[0]
-        print()
-        return {'motherboard': fabricante, 'model': model, 'manufacturer': manufacturer}
+        manufacturer = winreg.QueryValueEx(chave, "BaseBoardManufacturer")[0]
+        return {'model_extend': model_extend, 'model': model, 'manufacturer': manufacturer}
     except Exception as e:
         print("err",e)
         return {'motherboard': "Máquina Virtual Detected", 'model': "Máquina Virtual Detected"}
-    
 
 def bytes_para_gb(bytes_valor):
     return round(bytes_valor / (1024 ** 3), 2)
 
 def verify_system(returnValue):
-    if(platform.system() == "Windows"):
+    if platform.system() == "Windows":
         return returnValue
 
 def get_cpu_info():
     try:
-        # Obter o modelo exato do processador no Windows
         if platform.system() == "Windows":
             process = subprocess.run(['wmic', 'cpu', 'get', 'name'], capture_output=True, text=True, shell=True)
             if process.returncode == 0:
                 model = process.stdout.strip().split('\n')[2]
             else:
                 raise Exception(process.stderr.strip())
-
-        # Obter informações genéricas sobre a CPU para outros sistemas
         else:
             model = platform.processor()
-
         return {
             "model": model,
             "architecture": platform.architecture()[0],
@@ -322,6 +199,7 @@ def get_network(ip):
         "ipv4": ip,
         "mac": get_mac_address()
     }
+
 def get_so_info():
     return {
         "so": get_version_system(),
@@ -360,7 +238,78 @@ def start_collect(client, identifiers):
         "uid": identifiers["uid"]
     }
 
-    # print(data)
     client.sendall(json.dumps(data).encode())
 
-run_client()
+def run_client():
+    server_ip = "192.168.0.2"  # Endereço IP do servidor
+    server_port = 8080  # Porta para o servidor socket
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((server_ip, server_port))
+    server_ip, server_port = client.getsockname()
+
+    hwid = get_hwid()        
+    uid = read_from_registry() or write_to_regedit(str(uuid4()))
+    start_collect(client, {"hwid": hwid, "uid": uid})    
+
+    client.close()
+
+    while True:
+        try:
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind((server_ip, server_port))
+            server.listen(1)
+
+            print("Server is waiting for connection...")
+
+            client, client_address = server.accept()
+            print(f"Connected to server, Client: {client_address}")
+            
+            command = client.recv(1024).decode()
+            print("command", command)
+
+            if command.strip().lower() == "shutdown_now":
+                print("Recebido comando de desligamento. Desligando a máquina...")
+                subprocess.run(["shutdown", "-p"], shell=True)
+            elif command.strip().lower() == "get_inventory":
+                print("get_inventory")
+                start_collect(client, {"hwid": hwid, "uid": uid})
+            elif command.strip().lower() == "quit":
+                print("saindo...")
+                break
+            elif command.strip().lower() == "winget_install":
+                subprocess.run("powershell -command \"iex ((New-Object System.Net.WebClient).DownloadString('https://aka.ms/install-winget'))\"")
+            elif command.strip().lower() == "remove_agent":
+                subprocess.Popen(["wmic product where name='nameApp' call uninstall"])
+            else:
+                timeout = command.split("##")[1] if len(command.split("##")) > 1 else 30
+                command = command.split("##")[0]
+
+                if "echo" in command:
+                    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.bat') as temp_file:
+                        temp_file.write(command)
+                        command = temp_file.name
+
+                encoding = "cp850" if platform.system() == "Windows" else "utf_8"
+                try:
+                    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True, encoding=encoding)
+                    stdout, stderr = process.communicate(timeout=int(timeout))
+                    if stdout:
+                        client.sendall(stdout.encode())
+                    if stderr:
+                        client.sendall(stderr.encode())
+                        print("Erro ao executar o comando:", stderr)
+                except subprocess.TimeoutExpired:
+                    print("Tempo limite excedido ao executar o comando.")
+                except Exception as e:
+                    print("Erro ao executar o comando:", e)
+
+        except Exception as e:
+            print(f"Erro: {e}")
+        finally:
+            client.close()
+            server.close()
+            print("Connection to client closed")
+
+if __name__ == "__main__":
+    run_client()
